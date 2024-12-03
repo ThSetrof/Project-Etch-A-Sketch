4be08d9f5dac99110eb262eb8836246b618b84dd
Tommy The Turtle <75889955+ThSetrof@users.noreply.github.com>
2024-12-03 12:12:32 +0000

    Add hover effect to pixel

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+
+let pixelPerRow = 16;
+let pixelHoverColor = 'darkslategray';
+let pixelGrid = createPixelGrid(pixelPerRow, pixelPerRow);
+
+
+
 function createElement(tag){
     let element = document.createElement(tag);
     return element;
 }
 
 
-let pixelPerRow = 16
 
 
 
@@ -19,6 +25,7 @@ function createPixelGrid(x, y){
         for(let j = 0; j < y; j++){
             let pixel = document.createElement('div');
             pixel.classList.add('pixel');
+            pixel.addEventListener('mouseover', changePixelColor);
             row.appendChild(pixel);
         }
 
@@ -29,9 +36,12 @@ function createPixelGrid(x, y){
 }
 
 
+function changePixelColor(e){
+    let pixel = e.target;
+    pixel.style.backgroundColor = pixelHoverColor;
+}
 
-let pixelGrid = createPixelGrid(pixelPerRow, pixelPerRow);
-let pixelColor = 'pink';
+
 
 document.body.appendChild(pixelGrid);
 
