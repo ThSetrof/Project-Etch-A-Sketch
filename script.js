ebff818ca76e31f0293f932b41d1b21ba993564e
Tommy The Turtle <75889955+ThSetrof@users.noreply.github.com>
2024-12-03 15:45:37 +0000

    Add buttom to change nr of pixels on the grid

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 
-let pixelPerRow = 16;
+let pixelPerRow = 10;
+
 let pixelHoverColor = 'darkslategray';
-let pixelGrid = createPixelGrid(pixelPerRow, pixelPerRow);
+
+let grid = document.querySelector('.pixelGrid');
+var rootStyle = getComputedStyle(document.querySelector(':root'));
+const gridPadding = parseInt(rootStyle.getPropertyValue('--grid-padding'));
+const gridWidth = parseInt(rootStyle.getPropertyValue('--grid-width'));
+const gridGap = parseInt(rootStyle.getPropertyValue('--grid-gap'));
+const pixelBtn = document.querySelector('button');
+
+pixelBtn.addEventListener('click', e => {
+    let n = parseInt(prompt('Enter number of pixel: '))
+    grid.textContent = '';
+    createPixelGrid(n)
+
+})
 
 
 
@@ -10,31 +24,26 @@ function createElement(tag){
     return element;
 }
 
+function createPixelGrid(pixelPerRow){
 
-
-
-
-function createPixelGrid(x, y){
-    let pixelGrid = createElement('div');
-    pixelGrid.classList.add('pixelGrid');
+    let pixelSize = getPixelSize(gridWidth, gridPadding, gridGap, pixelPerRow);
     
-    for(let i = 0; i < x; i++){
+    for(let i = 0; i < pixelPerRow; i++){
         let row = createElement('div');
         row.classList.add('row');
 
-        for(let j = 0; j < y; j++){
+        for(let j = 0; j < pixelPerRow; j++){
             let pixel = document.createElement('div');
             pixel.classList.add('pixel');
+            pixel.style.height = pixel.style.width = `${pixelSize}px`;
+
             pixel.addEventListener('mouseover', changePixelColor);
             row.appendChild(pixel);
         }
 
-        pixelGrid.appendChild(row);
+        grid.appendChild(row);
     }
-
-    return pixelGrid;
 }
-
 
 function changePixelColor(e){
     let pixel = e.target;
@@ -43,7 +52,16 @@ function changePixelColor(e){
 
 
 
-document.body.appendChild(pixelGrid);
 
-console.log(pixelGrid);
+function getPixelSize(width, padding, gap, pixelPerRow){
+    console.log(width, padding, gap, pixelPerRow );
+
+    const pixelSize = (width - (padding * 2) - (2 * gap * pixelPerRow)  ) / pixelPerRow;
+    return pixelSize;
+}
+
+
+createPixelGrid(pixelPerRow);
+
+
 
