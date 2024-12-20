3bfc687efb854cb14af32ab2a249cb754bd938ba
thsetrof <75889955+ThSetrof@users.noreply.github.com>
2024-12-20 17:35:53 +0000

    Add range input to be able to change the number of pixels in the grid.

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-
-let pixelPerRow = 10;
-
 let pixelHoverColor = 'darkslategray';
+let pixelPerRow = 16
 
 let grid = document.querySelector('.pixelGrid');
+const pixels = [];
 let rootStyle = getComputedStyle(document.querySelector(':root'));
 const gridPadding = parseInt(rootStyle.getPropertyValue('--grid-padding'));
 const gridWidth = parseInt(rootStyle.getPropertyValue('--grid-width'));
 const gridGap = parseInt(rootStyle.getPropertyValue('--grid-gap'));
 
-
 const pixelSizeInput = document.getElementById('pixelSize');
-console.log(pixelSizeInput.value);
 
+pixelSizeInput.addEventListener("input", event => {
 
+    let output = pixelSizeInput.previousElementSibling;
+    output.textContent = pixelSizeInput.value;
+});
 
+pixelSizeInput.addEventListener("change", event => {
+    pixelPerRow = Number.parseFloat(pixelSizeInput.value);
+    grid.textContent = '';
+    createPixelGrid(pixelPerRow);
+
+});
 
 function createElement(tag){
     return document.createElement(tag);
@@ -23,7 +30,8 @@ function createElement(tag){
 function createPixelGrid(pixelPerRow){
 
     let pixelSize = getPixelSize(gridWidth, gridPadding, gridGap, pixelPerRow);
-    
+    grid.textContent = '';
+
     for(let i = 0; i < pixelPerRow; i++){
         let row = createElement('div');
         row.classList.add('row');
@@ -35,9 +43,12 @@ function createPixelGrid(pixelPerRow){
 
             pixel.addEventListener('mouseover', changePixelColor);
             row.appendChild(pixel);
+            pixels.push(pixel);
         }
 
+
         grid.appendChild(row);
+
     }
 }
 
@@ -46,15 +57,16 @@ function changePixelColor(e){
     pixel.style.backgroundColor = pixelHoverColor;
 }
 
-
-
 function getPixelSize(width, padding, gap, pixelPerRow){
     return  (width - padding * 2) / pixelPerRow;
 }
 
+function clearGrid(){
+    pixels.forEach(pixel => pixel.remove());
+}
 
 createPixelGrid(pixelPerRow);
 
-console.log(pixelPerRow);
+
 
 
